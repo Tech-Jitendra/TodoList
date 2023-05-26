b2ab6c711cc5dd604a9231e73012a493c2c4de79
JACK99-JACK99 <jitenmaurya0@gmail.com>
2023-05-26 10:33:01 +0530

    duplicate model added

--- src/models/api/endpoint.test.types.ts
+++
@@ -1,6 +0,0 @@
-export const BASE_MODEL_TEST = {
-  id: expect.any(String),
-  created_on: expect.any(Date),
-  edited_on: expect.any(Date),
-  //_data:expect.toBeOneOf([[expect.anything(), null,undefined]])
-}
